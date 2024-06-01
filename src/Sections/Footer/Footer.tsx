ab1aeb1ea91cda53bc0b7805d98c619ae9e786a9
Artemii <latovgor@gmail.com>
2024-06-01 11:44:42 +0300

    gfasgahgsd3

--- src/Sections/Footer/Footer.tsx
+++ src/Sections/Footer/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
             <p className="Footer-p">Office / Lager: Ziegelstr. 2 10117 Berlin</p>
         </div>
         <div className="Footer-wrap-NUM">
-            <img src="src\assets\Footer\4. Круг, обводка, большая.svg" alt="" />
+            <img src="src/assets/Footer/4. Круг, обводка, большая.svg" alt="" />
             <div>
                 <p className="Footer-p">Tel.: +49 30 40790404</p>
                 <p className="Footer-p">Mob.: +49 179 5500504</p>

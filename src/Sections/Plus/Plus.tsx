91c6e1eb61595cd7b8a8a3970228c000be8e4854
Artemii <latovgor@gmail.com>
2024-06-05 01:22:56 +0300

    head2

--- src/Sections/Plus/Plus.tsx
+++ src/Sections/Plus/Plus.tsx
@@ -1,4 +1,8 @@
 import PlusCard from "../../Component/PlusCard/PlusCard"
+import Icon1 from "../../assets/Four/Category.svg"
+import Icon2 from "../../assets/Four/Category-1.svg"
+import Icon3 from "../../assets/Four/Category-2.svg"
+import Icon4 from "../../assets/Four/Category-3.svg"
 import "./Plus.css"
 
 const Plus = () => {
@@ -8,13 +12,13 @@ const Plus = () => {
             <h1 className="Plus-h1">Vorteile des Geschäfts</h1>
         </div>
         <div className="Plus-list">
-            <PlusCard Title="Große Auswahl" Img="src\assets\Four\Category.svg" Text="Wir haben eine breite Palette von Fahrrädern 
+            <PlusCard Title="Große Auswahl" Img={Icon1} Text="Wir haben eine breite Palette von Fahrrädern 
             für alle Altersgruppen und Fahrstilrichtungen." />
-            <PlusCard Title="Professionelle Beratung" Img="src\assets\Four\Category-1.svg" Text="Unser Expertenteam steht Ihnen 
+            <PlusCard Title="Professionelle Beratung" Img={Icon2} Text="Unser Expertenteam steht Ihnen 
             jederzeit zur Verfügung, um Ihnen bei der Auswahl zu helfen und alle Fragen zu beantworten." />
-            <PlusCard Title="Qualität" Img="src\assets\Four\Category-2.svg" Text="Wir bieten nur hochwertige Fahrräder 
+            <PlusCard Title="Qualität" Img={Icon3} Text="Wir bieten nur hochwertige Fahrräder 
             von vertrauenswürdigen Herstellern." />
-            <PlusCard Title="Schnelle Lieferung" Img="src\assets\Four\Category-3.svg" Text="Wir liefern Ihr neues Fahrrad direkt 
+            <PlusCard Title="Schnelle Lieferung" Img={Icon4} Text="Wir liefern Ihr neues Fahrrad direkt 
             vor Ihre Haustür." />
         </div>
     </div>
--- src/Sections/Sort/Sort.tsx
+++ src/Sections/Sort/Sort.tsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import Button from "../../Component/Button/Button"
 import VeloCard from "../../Component/VeloCard/VeloCard"
+
+import Cervélo from "../../assets/Three/Cervélo CALEDONIA-5.png"
+import Cervélo2 from "../../assets/Three/Cervélo S5 Disc.png"
+import Cinelli from "../../assets/Three/Cinelli HOBOOTLEG GEO.png"
+import CUBE from "../../assets/Three/CUBE NUROAD Pro FE.png"
+import FOCUS from "../../assets/Three/FOCUS ATLAS 8-9.png"
+import CUBE2 from "../../assets/Three/CUBE NUROAD WS.png"
+
 import "./Sort.css"
 
 const Sort = () => {
@@ -17,46 +25,44 @@ const Sort = () => {
             (<>
             <div className="Sort-wrapper">
         <VeloCard Text="Cervélo CALEDONIA-5 - Rival eTap AXS - Carbon Rennrad - 2024 five black" 
-            Img="src\assets\Three\Cervélo CALEDONIA-5.png" Price="4.699,00" />
+            Img={Cervélo} Price="4.699,00" />
 
         <VeloCard Text="Cervélo S5 Disc - Ultegra DI2 Carbon Rennrad - 2022 - iron oxide" 
-            Img="src\assets\Three\Cervélo S5 Disc.png" Price="8.999,00" />
+            Img={Cervélo2} Price="8.999,00" />
 
         <VeloCard Text="Cinelli HOBOOTLEG GEO - Shimano Tiagra Gravel Bike - 2023 - orange" 
-            Img="src\assets\Three\Cinelli HOBOOTLEG GEO.png" Price="2.299,99" />
-
+            Img={Cinelli} Price="2.299,99" />
 
         <VeloCard Text="CUBE NUROAD Pro FE - Gravel Bike - 2023 - metalblack / grey" 
-            Img="src\assets\Three\CUBE NUROAD Pro FE.png" Price="1.289,00" />
+            Img={CUBE} Price="1.289,00" />
 
         <VeloCard Text="FOCUS ATLAS 8.9 - Carbon Gravel Bike - 2023 - Gold Brown" 
-            Img="src\assets\Three\FOCUS ATLAS 8-9.png" Price="3.399,00" />
+            Img={FOCUS} Price="3.399,00" />
 
         <VeloCard Text="CUBE NUROAD WS - Damen Gravel Bike - 2023 - lightgrey / rose" 
-            Img="src\assets\Three\CUBE NUROAD WS.png" Price="4.699,00" />
+            Img={CUBE2} Price="4.699,00" />
       </div>
             </>)
             :
             (<>
-            <div className="Sort-wrapper">
-        <VeloCard Text="Cervélo CALEDONIA-5 - Rival eTap AXS - Carbon Rennrad - 2024 five black" 
-            Img="src\assets\Three\Cervélo CALEDONIA-5.png" Price="4.699,00" />
+        <div className="Sort-wrapper">
+            <VeloCard Text="Cervélo CALEDONIA-5 - Rival eTap AXS - Carbon Rennrad - 2024 five black" 
+            Img={Cervélo} Price="4.699,00" />
 
-        <VeloCard Text="Cervélo S5 Disc - Ultegra DI2 Carbon Rennrad - 2022 - iron oxide" 
-            Img="src\assets\Three\Cervélo S5 Disc.png" Price="8.999,00" />
+            <VeloCard Text="Cervélo S5 Disc - Ultegra DI2 Carbon Rennrad - 2022 - iron oxide" 
+            Img={Cervélo2} Price="8.999,00" />
 
-        <VeloCard Text="Cinelli HOBOOTLEG GEO - Shimano Tiagra Gravel Bike - 2023 - orange" 
-            Img="src\assets\Three\Cinelli HOBOOTLEG GEO.png" Price="2.299,99" />
+            <VeloCard Text="Cinelli HOBOOTLEG GEO - Shimano Tiagra Gravel Bike - 2023 - orange" 
+            Img={Cinelli} Price="2.299,99" />
 
+            <VeloCard Text="CUBE NUROAD Pro FE - Gravel Bike - 2023 - metalblack / grey" 
+            Img={CUBE} Price="1.289,00" />
 
-        <VeloCard Text="CUBE NUROAD Pro FE - Gravel Bike - 2023 - metalblack / grey" 
-            Img="src\assets\Three\CUBE NUROAD Pro FE.png" Price="1.289,00" />
+            <VeloCard Text="FOCUS ATLAS 8.9 - Carbon Gravel Bike - 2023 - Gold Brown" 
+            Img={FOCUS} Price="3.399,00" />
 
-        <VeloCard Text="FOCUS ATLAS 8.9 - Carbon Gravel Bike - 2023 - Gold Brown" 
-            Img="src\assets\Three\FOCUS ATLAS 8-9.png" Price="3.399,00" />
-
-        <VeloCard Text="CUBE NUROAD WS - Damen Gravel Bike - 2023 - lightgrey / rose" 
-            Img="src\assets\Three\CUBE NUROAD WS.png" Price="4.699,00" />
+            <VeloCard Text="CUBE NUROAD WS - Damen Gravel Bike - 2023 - lightgrey / rose" 
+            Img={CUBE2} Price="4.699,00" />
       </div>
       <div className="Sort-btn">
         <Button Text="MORE" onClick={() => {setShowMore(true)}}/>
